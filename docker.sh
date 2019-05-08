f7f245b20767e63354c3ac5dd096600374f4426a
ajinkyakadam <ajenkya.kadam@gmail.com>
2019-05-08 13:43:35 -0500

    container version indexd

---
+++ docker.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+set -euo pipefail
+
+rm -rf build/src build/src-editable
+mkdir -p build/src build/src-editable
+
+pip install \
+    --src build/src \
+    --no-deps \
+    $(grep '^-e' requirements.txt)
+
+cp requirements.txt build/requirements.txt
+sed -i.tmp \
+  -e 's;^-e.*egg=;build/src/;' \
+  -e '/^build\/src/s/_/-/g' \
+build/requirements.txt
+
+
+# avoid installing git
+COMMIT=`git rev-parse HEAD` && echo "COMMIT=\"${COMMIT}\"" >indexd/index/version_data.py
+VERSION=`git describe --always --tags` && echo "VERSION=\"${VERSION}\"" >>indexd/index/version_data.py
+
+
+docker build -t indexd .
+
+# If this is not a pull request, update the branch's docker tag.
+if [ $TRAVIS_PULL_REQUEST = 'false' ]; then
+  docker tag indexd quay.io/ncigdc/indexd:${TRAVIS_BRANCH/\//_} \
+    && docker push quay.io/ncigdc/indexd:${TRAVIS_BRANCH/\//_};
+
+  # If this commit has a tag, use on the registry too.
+  if ! test -z $TRAVIS_TAG; then
+    docker tag indexd quay.io/ncigdc/indexd:${TRAVIS_TAG} \
+      && docker push quay.io/ncigdc/indexd:${TRAVIS_TAG};
+  fi
+fi
--- wsgi.py
+++ wsgi.py
@@ -1,3 +1,5 @@
+import sys
+sys.path.append('/var/www/indexd')
 from indexd import get_app
 import os
 os.environ['INDEXD_SETTINGS'] = '/var/www/indexd/'
