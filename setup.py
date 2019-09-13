42602c50fdeaa098b89d56c9b3fc36f17c09ead3
Patrick Huarng <huarngpa@gmail.com>
2019-09-12 23:37:16 -0500

    fix(deps): py3 compatible deps

--- setup.py
+++ setup.py
@@ -12,9 +12,9 @@ setup(
     install_requires=[
         'flask==0.12.4',
         'jsonschema==2.5.1',
-        'sqlalchemy==1.0.8',
+        'psycopg2-binary==2.8.2',
+        'sqlalchemy==1.3.3',
         'sqlalchemy-utils>=0.32.21',
-        'psycopg2>=2.7',
         'future>=0.16.0,<1.0.0',
         'indexclient',
     ],
