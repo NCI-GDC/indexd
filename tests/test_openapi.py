64f97b586a62f860206356043c8fbc5634916801
Michael Solano <96085518+mpsolano@users.noreply.github.com>
2022-02-17 11:46:24 -0600

    DEV-203: Update import statements and package usage to follow best practices

--- tests/test_openapi.py
+++ tests/test_openapi.py
@@ -1,20 +1,20 @@
 import json
 import pkg_resources
 
-from openapi_spec_validator import validate_spec
-from openapi_spec_validator.exceptions import OpenAPIValidationError
-from openapi_spec_validator.readers import read_from_filename
+import openapi_spec_validator
+from openapi_spec_validator import exceptions as specs_exceptions
+from openapi_spec_validator import readers as specs_readers
 
 import openapis
 
 
 def test_valid_openapi():
     filename = pkg_resources.resource_filename(openapis.__name__, 'swagger.yaml')
-    spec, url = read_from_filename(filename)
+    spec, url = specs_readers.read_from_filename(filename)
 
     if not isinstance(spec, dict):
-        raise OpenAPIValidationError('root node is not a mapping')
+        raise specs_exceptions.OpenAPIValidationError('root node is not a mapping')
     # ensure the spec is valid JSON
     spec = json.loads(json.dumps(spec))
 
-    validate_spec(spec, url)
+    openapi_spec_validator.validate_spec(spec, url)
