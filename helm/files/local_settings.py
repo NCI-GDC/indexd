decc0b4cc9fc01e607736a3e76de9b2a2dc546ce
Arthur Collet <acollet@uchicago.edu>
2022-03-03 17:02:45 -0600

    Tox build hangs

--- helm/files/local_settings.py
+++ helm/files/local_settings.py
@@ -10,7 +10,7 @@ from indexd.auth.drivers.alchemy import SQLAlchemyAuthDriver
 URL_STRING = 'postgresql://{pg_user}:{pg_pass}@{pg_host}:{pg_port}/{pg_name}'.format(
     pg_user=os.getenv("PG_USER", ""),
     pg_pass=os.environ.get("PG_PASS"),
-    pg_host=os.environ.get("PG_HOST"),
+    pg_host=os.environ.get("PG_HOST", "postgres"),
     pg_port=int(os.getenv("PG_PORT", "5432")),
     pg_name=os.getenv("PG_DATABASE", ""),
 )
