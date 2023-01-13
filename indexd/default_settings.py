aff92a9883aac48190a5f2bc20d728927e54d841
Qiao Qiao <qiaoq@uchicago.edu>
2023-01-13 10:50:21 -0600

    use string format for python2.7

--- indexd/default_settings.py
+++ indexd/default_settings.py
@@ -12,7 +12,7 @@ SQLALCHEMY_VERBOSE = (
     os.getenv('INDEXD_VERBOSE', '').lower() in ['1', 'yes', 'true']
 )
 PG_HOST = os.getenv("PG_INDEXD_HOST", "localhost")
-PG_URL = f'postgresql://test:test@{PG_HOST}/indexd_test'
+PG_URL = 'postgresql://test:test@{PG_HOST}/indexd_test'.format(PG_HOST)
 
 CONFIG['INDEX'] = {
     'driver': SQLAlchemyIndexDriver(
