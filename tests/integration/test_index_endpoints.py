839c92e632a05fe167967aa179335cac6336d45d
Rowland Ogwara <rogwara@uchicago.edu>
2024-04-26 15:30:45 -0500

    fix test

--- tests/integration/test_index_endpoints.py
+++ tests/integration/test_index_endpoints.py
@@ -6,4 +6,4 @@ def test_index_get(indexd_systems_api):
     version_data = indexd_systems_api.version_get()
     version = Version(version_data.version)
     assert version.major is not None
-    assert len(version_data.commit) == 7
+    assert len(version_data.commit) > 6
