08245ddf2cfe2ddb01b2aee517a38361dd009948
Rowland Ogwara <rogwara@uchicago.edu>
2024-10-14 14:29:25 -0500

    proxy

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ setup(
         "sqlalchemy-utils>=0.32",
         "psycopg2>=2.7",
         "cdislogging>=1.0",
-        "requests>=2.32.3",  # pinned for DEV-2844 (POAM)
+        "requests>=2.32.2",  # pinned for DEV-2844 (POAM)
         "ddtrace>=2.9.1",
         "importlib-metadata>=1.4",
         # jsonschema-spec 0.1.6 depends on typing-extensions<4.6.0
