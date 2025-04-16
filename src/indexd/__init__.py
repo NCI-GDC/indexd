1b46dbd00ed60c91db44162a01a6994ed2bc7424
Rowland Ogwara <rogwara@uchicago.edu>
2025-04-16 13:17:13 -0500

    use recipies

--- src/indexd/__init__.py
+++ src/indexd/__init__.py
@@ -1,7 +1,6 @@
 from importlib.metadata import distribution
 
 import logstick
-from ddtrace import auto  # noqa
 
 logstick.configure_logging(
     namespace=__name__, disable_existing_loggers=False, extra_namespaces=["werkzeug"]
